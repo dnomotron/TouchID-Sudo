52ce7bb7ffc859223f8f7684646524ae82cc0baf
Dalton Rick <dtr@apple.com>
2022-09-22 11:37:26 -0600

    Updated addtouch to cleanup logic
    zsh (and newer bash) support evaluating the return code of a command
    directly in an if statement, removing the need for an explict check of
    `$?` which improves readability

--- addTouch.sh
+++ addTouch.sh
@@ -2,7 +2,6 @@
 
 exec 1> /tmp/addtouch.log 2>&1
 
-grep -e '^auth       sufficient     pam_tid.so$' /etc/pam.d/sudo
-if [ $? -ne 0 ]; then
+if ! grep -e '^auth       sufficient     pam_tid.so$' /etc/pam.d/sudo; then
 	sudo sed -E -i '.bak' '1s/^(#.*)$/\1\nauth       sufficient     pam_tid.so/' /etc/pam.d/sudo
 fi
